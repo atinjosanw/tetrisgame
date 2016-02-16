da0c91ab54f090228b7fe0ddce99a505ff78955e
Yating Wang <atinjosan@yahoo.com>
2016-02-16 17:52:51 -0500

    Finish frontpage in angular

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 'use strict';
-let express = require('express');
-let app = express();
-let path = require('path');
+let app = require('./app');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
-let listener = app.listen(process.env.PORT || 8080, function() {
-    console.log("Server is listening on port " + listener.address().port);
-});
-app.use(function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.set('port', process.env.PORT_BACKEND || 8080);
 
+let server = app.listen(app.get('port'), function() {
+    console.log("Server is listening on port " + server.address().port + ' ... ');
+});
