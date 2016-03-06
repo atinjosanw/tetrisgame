a526048f5ecccc4dd1174517a2833a0203726706
Yating Wang <atinjosan@yahoo.com>
2016-03-06 11:28:40 -0500

    Finish authentication middleware with database connection

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,26 +5,29 @@ let constant = require('./share/constants');
 
 const restAuth = function (req, res, next) {
     if (req.session && req.session.user) {
-        queryDB.findUser({email: req.session.user.email}, function (err, user) {
-            if (user) {
+        queryDB.findUser({email: req.session.user.email})
+        .then(user => {
+            if (user != null) {
                 req.user = user;
                 req.session.user = user;
             }
-            next();
+            return next();
         });
-    }
+    };
     next();
-};
+}
 
-let socketAuth = function (socket, next) {
+const socketAuth = function (socket, next) {
     let cookie = socket.request.headers.cookie;
+    if (!cookie) {
+        return next();
+    }
     let parseCookie = cookieParser(constant.SESS_SECRET);
     socket.cookie = parseCookie(cookie);
     socket.sessionID = socket.cookie.sessionID;
     redisSessionClient.get("sess:" + socket.sessionID, function (err, sess) {
         if (!err && sess){
             socket.user = sess.user;
-            next();
         }
         next();
     });
--- share/queryDB.js
+++ share/queryDB.js
@@ -1,13 +1,34 @@
 'use strict';
-let pg = require('pg');
 const conString = require('./constants').PG_CONNECTION;
+let pgp = require('pg-promise')();
+const db = pgp(conString);
 
-const findUser = function (query, callback) {
-    const email = query.email;
-    const password = query.password;
-    // TODO
+
+const findUser = function (query) {
+    return
+    Promise
+    .resolve(db.one("SELECT * FROM users WHERE user_email=${email~}", query))
+    .then(user => {
+            return user;
+    })
+    .catch(error => {
+        throw new Error(error);
+    });
+};
+
+const insertUser = function (query) {
+    return Promise
+    .resolve(db.one("INSERT INTO users(user_email, user_password)" +
+                    "VALUES (${email~}, ${password~})", query)
+    .then(user => {
+        return user;
+    })
+    .catch(error => {
+        throw new Error(error);
+    });
 }
 
 
 module.exports.findUser = findUser;
+module.exports.insertUser = insertUser;
 
