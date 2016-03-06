8941b3c334f67520a0b3ab7a1bda3e7b484ce89f
Yating Wang <atinjosan@yahoo.com>
2016-03-06 00:34:06 -0500

    Organize constants

---
+++ share/constants.js
@@ -0,0 +1,7 @@
+const COOKIE_SECRET = 'cookiesecret';
+const SESS_SECRET = '123456789ABC';
+const PG_CONNECTION = 'postgres://tetris:tetris@localhost:5432/tetris';
+
+module.exports.COOKIE_SECRET = COOKIE_SECRET;
+module.exports.SESS_SECRET = SESS_SECRET;
+module.exports.PG_CONNECTION = PG_CONNECTION;
