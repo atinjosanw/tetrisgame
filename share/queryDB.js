1ae5be03e9228b52d4b063e0a50c267c1319e358
Yating Wang <atinjosan@yahoo.com>
2016-03-12 11:35:43 -0500

    Add postgres user deletion

--- share/queryDB.js
+++ share/queryDB.js
@@ -1,36 +1,40 @@
 'use strict';
-const cn = require('./constants').PG_CONNECTION;
-let pgp = require('pg-promise')(cn);
-const db = pgp(conString);
-
-
-const findUser = function (query) {
-    return
-    Promise
-    .resolve(db.one("SELECT * FROM users WHERE user_email=${email~}", query))
-    .then(user => {
-            return user;
-    })
-    .catch(error => {
-        throw new Error(error);
-    })
-    .finally(pgp.end);
-};
-
-const insertUser = function (query) {
+const findUser = function (query, db) {
     return Promise
-    .resolve(db.one("INSERT INTO users(user_email, user_password)" +
-                    "VALUES (${email~}, ${password~})", query)
+    .resolve(db.one("SELECT user_email, user_password FROM users WHERE user_email=${email}", query))
     .then(user => {
         return user;
     })
     .catch(error => {
         throw new Error(error);
-    })
-    .finally(pgp.end);
-}
+    });
+};
 
+const insertUser = function (query, db) {
+    return Promise
+    .resolve(db.none("INSERT INTO users(user_email, user_password)" +
+                    "VALUES (${email}, ${password})", query))
+    .then(() => {
+        return {user_email: query.email, user_password: query.password};
+    })
+    .catch(error => {
+        throw new Error(error);
+    });
+};
+
+const deleteUser = function (query, db) {
+    return Promise
+    .resolve(db.result("DELETE FROM users where user_email=${email} " +
+                        "AND user_password=${password}", query))
+    .then(function (result) {
+        return result.rowCount;
+    })
+    .catch(function (error) {
+        throw new Error(error);
+    });
+};
 
 module.exports.findUser = findUser;
 module.exports.insertUser = insertUser;
+module.exports.deleteUser = deleteUser;
 
